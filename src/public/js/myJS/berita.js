c32ffdff5f9a52007cf0a64c5878fdcc6bf4361f
azizhadiid <azizalhadiid88@mgail.com>
2024-11-22 12:43:07 +0700

    main html]

--- src/public/js/myJS/berita.js
+++ src/public/js/myJS/berita.js
@@ -10,33 +10,37 @@ document.addEventListener("DOMContentLoaded", () => {
                 data.forEach((item) => {
                     const cardHTML = `
             <!-- Card 1 -->
-              <div class="mb-4 p-3" style="background-color: #F2F2F2; border-radius: 10px; width: 21rem; border: 1px solid black;">
-                <img src="${item.foto}" class="card-img-top custom-img" alt="" width="300" height="190">
-                <div class="">
-                  <h5 class="mt-3 mb-3" style="color: black; font-weight: bold;">${item.judul}</h5>
-                  <p class="card-text">${item.isi}</p>
-                  <div class="d-flex justify-content-end">
-                    <a href="" class="btn btn-success card-link mt-3" style="font-weight: bold;" data-toggle="modal"
-                      data-target="#modal-${item.idInformasi}">Lihat Detail</a>
+            <div class="mb-4 p-3 card-container" style="background-color: #F2F2F2; border-radius: 10px; width: 21rem; border: 1px solid black;">
+              <img src="${item.foto}" class="card-img-top custom-img" alt="" width="300" height="190">
+              <div class="card-content">
+                <!-- Judul dengan text truncation -->
+                <h5 class="mt-3 mb-3 card-title" style="color: black; font-weight: bold;">${item.judul}</h5>
+                <h6>${item.created.substring(0, 10)}</h6>
+                <p class="card-text">${item.isi}</p>
+                <div class="d-flex justify-content-end">
+                  <a href="" class="btn btn-success card-link mt-3" style="font-weight: bold;" data-toggle="modal"
+                    data-target="#modal-${item.idInformasi}">Lihat Detail</a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="modal-${item.idInformasi}" tabindex="-1" aria-labelledby="beritaLabeberita" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel" style="color: black;">${item.judul}</h5>
+                  </div>
+                  <div class="modal-body" style="color: black;">
+                    <h6>${item.created.substring(0, 10)}</h6>
+                    ${item.isi}
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal" style="font-weight: bold;">Tutup</button>
                   </div>
                 </div>
               </div>
-              <!-- Modal -->
-              <div class="modal fade" id="modal-${item.idInformasi}" tabindex="-1" aria-labelledby="beritaLabeberita" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel" style="color: black;">${item.judul}</h5>
-                    </div>
-                    <div class="modal-body" style="color: black;">
-                      ${item.isi}
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-danger" data-dismiss="modal" style="font-weight: bold;">Tutup</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            </div>
             <!-- End Card and Modal -->
             `;
                     cardContainer.innerHTML += cardHTML;
---
+++ src/public/js/myJS/layanan.js
@@ -0,0 +1,4 @@
+function getMassage() {
+    const layanan = document.getElementById("Layanan");
+    
+}
