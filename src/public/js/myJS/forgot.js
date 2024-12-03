b68559a3c522019d9141ff4a3d6416dc923e0786
azizhadiid <azizalhadiid88@mgail.com>
2024-12-03 13:46:25 +0700

    forgot

---
+++ src/public/js/myJS/forgot.js
@@ -0,0 +1,51 @@
+const form = document.querySelector("form");
+
+async function login() {
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const jsonData = Object.fromEntries(formData.entries());
+
+        try {
+            const response = await fetch("/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(jsonData),
+            });
+
+            if (response.ok) {
+                window.location.href = "/home";
+            } else if (response.status === 401) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Login Failed",
+                    text: "Nama pengguna atau kata sandi salah",
+                    timer: 3000,
+                    timerProgressBar: true,
+                });
+            } else {
+                const errorData = await response.json();
+                console.error("Error submitting form:", errorData || response.statusText);
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: errorData?.message,
+                });
+            }
+        } catch (error) {
+            console.error("Error submitting form:", error);
+            Swal.fire({
+                icon: "error",
+                title: "Kesalahan jaringan",
+                text: "Tidak dapat terhubung ke server, Periksa koneksi Anda dan coba lagi",
+            });
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    login();
+});
